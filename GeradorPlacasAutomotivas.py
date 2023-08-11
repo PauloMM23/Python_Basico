627fea9974a42c704df105d20c2fc4044220df7e
Paulo André Machado Maracci <92092995+PauloMM23@users.noreply.github.com>
2023-08-11 00:46:06 -0300

    Update GeradorPlacasAutomotivas.py

--- GeradorPlacasAutomotivas.py
+++ GeradorPlacasAutomotivas.py
@@ -3,15 +3,15 @@ import random
 
 def gerar_placa_automotiva():
     letra = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
-             "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"] #Ou letras = [chr(i) for i in range(65, 91)] #(69, 91) código ASCII referente as letras maiúsculas
-    numero = [str(i) for i in range(10)] #(range(10)) Lista de números de 0 a 9. str(i) converte número em string
+             "N","O","P","Q","R","S","T","U","V","W","X","Y","Z"] #Ou letras = [chr(i) for i in range(65, 91)] #(69, 91) codigo ASCII referente as letras maiusculas
+    numero = [str(i) for i in range(10)] #(range(10)) Lista de numeros de 0 a 9. str(i) converte numero em string
 
-    #insere letras e números aleatórios na seguinte sequência: LLLNLNN
+    #insere letras e numeros aleatorios na seguinte sequencia: LLLNLNN
     placa_carro = random.choice(letra) + random.choice(letra) + random.choice(letra) + \
     random.choice(numero) + random.choice(letra) + random.choice(numero) + random.choice(numero) 
     return placa_carro
 
-#usuário insere a quantidade de placas que deseja gerar
+#usuario insere a quantidade de placas que deseja gerar
 qtd_placas = int(input("Quantas placas deseja gerar? "))
 
 for i in range(qtd_placas):
